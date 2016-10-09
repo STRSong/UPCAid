716369132073321576fe2c1cce26e036be4d5f0b
xhaiben <xhaiben@hotmail.com>
2016-10-09 12:27:42 +0800

    修改ui

--- app/src/main/java/jwxt/cacher/cc/jwxt/WelcomeActivity.java
+++ app/src/main/java/jwxt/cacher/cc/jwxt/WelcomeActivity.java
@@ -40,7 +40,7 @@ public class WelcomeActivity extends Activity {
                public void run() {
                     //在子线程里停留三秒钟
                     try {
-                         Thread.sleep(3000);
+                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                          //把异常信息打印在控制台上
                          e.printStackTrace();
