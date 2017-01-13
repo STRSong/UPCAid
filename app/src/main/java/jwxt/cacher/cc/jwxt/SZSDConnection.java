a335782f79c5231963ce049a72d67a131b91249d
xhaiben <xhaiben@hotmail.com>
2017-01-13 22:18:27 +0800

    修改getSelfInfo

--- app/src/main/java/jwxt/cacher/cc/jwxt/SZSDConnection.java
+++ app/src/main/java/jwxt/cacher/cc/jwxt/SZSDConnection.java
@@ -126,32 +126,32 @@ public class SZSDConnection {
 //        return false;
 //    }
 
-    public Map<String, String> getSelfInfo() {
-        try {
-            URL url = new URL("http://192.168.0.11:8080/upcaid?command=getSelfInfo");
-            HttpURLConnection
-                    httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestProperty("szsdCookie", szsdCookie);
-            httpURLConnection.setDoInput(true);
-            httpURLConnection.setConnectTimeout(timeOut);
-            httpURLConnection.setReadTimeout(timeOut);
-            httpURLConnection.connect();
-            String str = CacherUtils.getHttpString(httpURLConnection);
-            JSONObject jsonObject = JSONObject.fromObject(str);
-            JSONArray jsonArray = (JSONArray) jsonObject.get("list");
-            jsonObject = jsonArray.getJSONObject(0);
-            jsonObject.get("map");
-            Map<String, String> infoMap = (Map<String, String>) jsonObject.get("map");
-
-            if (httpURLConnection != null) {
-                httpURLConnection.disconnect();
-            }
-            return infoMap;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    public Map<String, String> getSelfInfo() {
+//        try {
+//            URL url = new URL("http://192.168.0.11:8080/upcaid?command=getSelfInfo");
+//            HttpURLConnection
+//                    httpURLConnection = (HttpURLConnection) url.openConnection();
+//            httpURLConnection.setRequestProperty("szsdCookie", szsdCookie);
+//            httpURLConnection.setDoInput(true);
+//            httpURLConnection.setConnectTimeout(timeOut);
+//            httpURLConnection.setReadTimeout(timeOut);
+//            httpURLConnection.connect();
+//            String str = CacherUtils.getHttpString(httpURLConnection);
+//            JSONObject jsonObject = JSONObject.fromObject(str);
+//            JSONArray jsonArray = (JSONArray) jsonObject.get("list");
+//            jsonObject = jsonArray.getJSONObject(0);
+//            jsonObject.get("map");
+//            Map<String, String> infoMap = (Map<String, String>) jsonObject.get("map");
+//
+//            if (httpURLConnection != null) {
+//                httpURLConnection.disconnect();
+//            }
+//            return infoMap;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
     public Map<String, String> getLibAndCardInfo() {
         try {
@@ -163,11 +163,9 @@ public class SZSDConnection {
             httpURLConnection.setReadTimeout(timeOut);
             httpURLConnection.setDoInput(true);
             httpURLConnection.connect();
-            ObjectInputStream inputStream = new ObjectInputStream(httpURLConnection.getInputStream());
-            Map<String, String> a = (Map<String, String>) inputStream.readObject();
-            if (inputStream != null) {
-                inputStream.close();
-            }
+            String resqContent = CacherUtils.getHttpString(httpURLConnection);
+            JSONObject jsonObject = JSONObject.fromObject(resqContent);
+            Map<String, String> a = (Map<String, String>) jsonObject.get("map");
             if (httpURLConnection != null) {
                 httpURLConnection.disconnect();
             }
